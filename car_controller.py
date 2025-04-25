910d8bd50dd9bce4a547414fe8b0fd89eb76b65a
Andi Li <al4725@columbia.edu>
2025-04-25 00:15:48 -0400

    quit botton added

--- car_controller.py
+++ car_controller.py
@@ -15,10 +15,11 @@ except pygame.error: #else print a error msg
     
 #hat_number = pygame.get_numhats()
 
-running = True
 
-while running:
+while True:
     pygame.event.pump()
+    if(joystick.get_button(2)): 
+        break
     x_axis = joystick.get_axis(2) #right stick left -- right
     y_axis = joystick.get_axis(3) #right stick up -- down
     print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
