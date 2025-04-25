74f710114f8deffc14276cb9db03b3af11b92585
Andi Li <al4725@columbia.edu>
2025-04-25 00:20:22 -0400

    quit botton added

--- car_controller.py
+++ car_controller.py
@@ -18,8 +18,7 @@ except pygame.error: #else print a error msg
 
 while True:
     pygame.event.pump()
-    if(joystick.get_button(2)): 
-        pygame.quit()
+    exit_botton = joystick.get_button(2)
     x_axis = joystick.get_axis(2) #right stick left -- right
     y_axis = joystick.get_axis(3) #right stick up -- down
     print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
