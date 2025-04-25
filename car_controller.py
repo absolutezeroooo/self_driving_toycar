52e44773c742ae748740c6e9f4b5e85ade45b4ac
Andi Li <al4725@columbia.edu>
2025-04-25 01:10:21 -0400

    quit botton added

--- car_controller.py
+++ car_controller.py
@@ -1,34 +1,44 @@
 import pygame
 import time
-
-pygame.init()
-pygame.joystick.init() 
+from picarx import Picarx
 
 
-try: 
-    joystick = pygame.joystick.Joystick(0) #only one joystick is used in our case
-    joystick.init() #initialize joystick module
-    print("joystick connection established")
-except pygame.error: #else print a error msg
-    print("Joystick not found, please try again")
-    quit()
+def joystick_init():
+    try: 
+        joystick = pygame.joystick.Joystick(0) #only one joystick is used in our case
+        joystick.init() #initialize joystick module
+        print("joystick connection established")
+    except pygame.error: #else print a error msg
+        print("Joystick not found, please try again")
+        quit()
     
-#hat_number = pygame.get_numhats()
-# while True:
-#     pygame.event.pump()
-#     for i in range(joystick.get_numbuttons()):
-#         print(f"Button {i}: {joystick.get_button(i)}", end="  ")
-#     print()
-#     time.sleep(0.1)
+def car_control(move: float, direction: float):
+    if(move >= 0): 
+        px.forward(0)
+    else:
+        px.forward(40)
+        angle = int(direction * 40)
+        px.set_dir_servo_angle(angle)
 
-while True:
-    pygame.event.pump()
-    if(joystick.get_button(3)): pygame.quit()
-    x_axis = joystick.get_axis(2) #right stick left -- right
-    y_axis = joystick.get_axis(3) #right stick up -- down
-    print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
-    time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.joystick.init() 
+    global joystick
+    joystick_init()
+    px = Picarx()
     
-pygame.quit()
+    while True:
+        pygame.event.pump()
+        if(joystick.get_button(3)): 
+            break
+        x_axis = joystick.get_axis(2) #right stick left -- right
+        y_axis = joystick.get_axis(3) #right stick up -- down
+        print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
+        car_control(y_axis, x_axis)
+        time.sleep(0.1)
+        
+    pygame.quit()
 
 
