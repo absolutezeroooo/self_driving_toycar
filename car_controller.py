2054f8f8c7cf9c4fe1d8e89d9d26164cdcb7b405
Andi Li <al4725@columbia.edu>
2025-04-25 00:28:54 -0400

    inital test

--- car_controller.py
+++ car_controller.py
@@ -1,5 +1,5 @@
 import pygame
-
+import time
 
 pygame.init()
 pygame.joystick.init() 
@@ -14,14 +14,19 @@ except pygame.error: #else print a error msg
     quit()
     
 #hat_number = pygame.get_numhats()
-
-
 while True:
     pygame.event.pump()
-    exit_botton = joystick.get_button(2)
-    x_axis = joystick.get_axis(2) #right stick left -- right
-    y_axis = joystick.get_axis(3) #right stick up -- down
-    print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}, exit: {exit_botton}")
+    for i in range(joystick.get_numbuttons()):
+        print(f"Button {i}: {joystick.get_button(i)}", end="  ")
+    print()
+    time.sleep(0.1)
+
+# while True:
+#     pygame.event.pump()
+#     exit_botton = joystick.get_button(2)
+#     x_axis = joystick.get_axis(2) #right stick left -- right
+#     y_axis = joystick.get_axis(3) #right stick up -- down
+#     print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}, exit: {exit_botton}")
     
 pygame.quit()
 
