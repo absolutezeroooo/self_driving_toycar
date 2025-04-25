22dbd4d888c906e3625102efef4f2c3c185a9d76
Andi Li <al4725@columbia.edu>
2025-04-25 00:25:08 -0400

    quit botton added

--- car_controller.py
+++ car_controller.py
@@ -21,8 +21,7 @@ while True:
     exit_botton = joystick.get_button(2)
     x_axis = joystick.get_axis(2) #right stick left -- right
     y_axis = joystick.get_axis(3) #right stick up -- down
-    print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
-    print(exit_botton)
+    print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}, exit: {exit_botton}")
     
 pygame.quit()
 
