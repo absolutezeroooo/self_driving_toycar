84b074f5f9a239e3ac8d43b6a5faad779521f2f6
Andi Li <al4725@columbia.edu>
2025-04-26 15:57:33 -0400

    recording function added to a new file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a README file
+
+This directory includes the following files:
+car_controller.py: 
+collect_data.py: This is used to collect data while driving the car manually,  
---
+++ collect_data.py
@@ -0,0 +1,102 @@
+import pygame
+import time
+import os
+import csv
+from picarx import Picarx
+from vilib import Vilib
+
+
+class data_collector:
+    def __init__(self, data_folder, fps):
+        self.data_folder = data_folder
+        self.fps = fps
+        self.image_folder = os.path.join(self.data_folder, "saved_images") #path to the image folder
+        self.csv_path = os.path.join(self.data_folder, "controller_data.csv") #path to the csv file
+        
+        os.makedirs(self.data_folder, exist_ok=True) #make a data folder if does not exist one
+        
+        self.controller_data_csv = open(self.csv_path, mode='w', newline='')
+        self.csv_writer = csv.writer(self.controller_data_csv)
+        self.csv_writer.writerow(["image_name", "controller_data"])
+    
+        self.image_number = 0
+        self.start_time = time.time()
+        
+    def record_data(self, controller_data):
+        image_name = f"{self.image_number}.jpg"
+        image_path = os.path.join(self.image_folder, image_name)
+        
+        Vilib.take_photo(image_name, image_path)
+        self.csv_writer.writerow([image_name, controller_data])
+        
+        print(f"saving {image_name} with controller value{controller_data}")
+        self.image_number += 1
+        
+        
+    def driving_and_collect(self) -> None:
+        while(True):
+            pygame.event.pump()
+            if(joystick.get_button(1)):
+                print("stop button pressed, stop recording...")
+                break
+            else:
+                current_time = time.time()
+                x_axis = joystick.get_axis(2) #right stick left -- right
+                y_axis = joystick.get_axis(1) #controls moving forward or backward
+                car_control(y_axis, x_axis)
+                if(current_time - self.start_time < 1/self.fps):
+                    continue
+                else:    
+                    self.record_data(x_axis)
+                    self.start_time = current_time
+ 
+        print("Closing csv file...")
+        self.controller_data_csv.close()
+    
+def car_control(move: float, direction: float):
+    angle = int(direction * 30)
+    px.set_dir_servo_angle(angle)
+    print(f"angle: {angle}")
+    
+    if(move > 0.2): px.backward(40)
+    elif(move >= 0): 
+        px.forward(0)
+    else:
+        px.forward(40)
+
+
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.joystick.init() 
+    try: 
+        joystick = pygame.joystick.Joystick(0) #only one joystick is used in our case
+        joystick.init() #initialize joystick module
+        print("joystick connection established")
+    except pygame.error: #else print a error msg
+        print("Joystick not found, please try again")
+        quit()
+        
+    px = Picarx()
+    
+    while True:
+        pygame.event.pump()
+        if(joystick.get_button(3)): 
+            print("exit button pressed, quit...")
+            break
+        elif(joystick.get_button(0)):
+            print("Recording button pressed, start recording...")
+            collector = data_collector(save_folder="/home/driving_car_data", fps=5)
+            collector.driving_and_collect()
+            
+        x_axis = joystick.get_axis(2) #right stick left -- right
+        #y_axis = joystick.get_axis(3) #right stick up -- down
+        y_axis = joystick.get_axis(1)
+        #print(f"X: {x_axis:.4f}, Y: {y_axis:.4f}")
+        car_control(y_axis, x_axis)
+        time.sleep(0.1)
+   
+    px.stop()  
+    pygame.quit()
+    
