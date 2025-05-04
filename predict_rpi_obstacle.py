863eec61be337389bad91ffbf6e56a84a056cf7e
Andi Li <al4725@columbia.edu>
2025-05-03 20:00:07 -0400

    obstacle avoidence update

--- predict_rpi_obstacle.py
+++ predict_rpi_obstacle.py
@@ -24,7 +24,7 @@ def obstacle_avoidence(distance: int):
     if(distance <= 15):  
         px.set_dir_servo_angle(0)
         px.backward(backward_speed)
-        time.sleep(1)
+        time.sleep(1.2)
         
     elif(distance <= 25):
         px.set_dir_servo_angle(25)
